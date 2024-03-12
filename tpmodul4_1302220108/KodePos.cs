982143134d444bb70bde1ce75f27583f9380c8bb
Hp <Hp@LAPTOP-P71GJDB6>
2024-03-13 00:14:47 +0700

    Add project files.

---
+++ tpmodul4_1302220108/KodePos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302220108
+{
+    internal class KodePos
+    {
+        public String kelurahan;
+        public int kodepos;
+        public List<KodePos> listKodepost = new List<KodePos>();
+        public KodePos(String a = "", int k = 0)
+        {
+            this.kelurahan = a;
+            this.kodepos = k;
+        }
+
+        public void setKodePost()
+        {
+            this.listKodepost.Add(new KodePos("Batununggal", 40266));
+            this.listKodepost.Add(new KodePos("Kujangsar", 40287));
+            this.listKodepost.Add(new KodePos("Mengger", 40267));
+            this.listKodepost.Add(new KodePos("Wates", 40256));
+            this.listKodepost.Add(new KodePos("Cijaura", 40287));
+            this.listKodepost.Add(new KodePos("Jatisar", 40286));
+            this.listKodepost.Add(new KodePos("Margasar", 40286));
+            this.listKodepost.Add(new KodePos("Sekejati", 40286));
+            this.listKodepost.Add(new KodePos("Kebonwaru", 40272));
+            this.listKodepost.Add(new KodePos("Maleer", 40274));
+            this.listKodepost.Add(new KodePos("Samoja", 40273));
+        }
+
+        public void getKodePos()
+        {
+            Console.WriteLine("Kelurahan\tKode Pos");
+            foreach (KodePos k in this.listKodepost)
+            {
+                string kelPad = k.kelurahan.PadRight(15);
+                Console.Write(kelPad + "\t" + k.kodepos);
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
---
+++ tpmodul4_1302220108/Program.cs
@@ -0,0 +1,23 @@
+﻿using tpmodul4_1302220108;
+
+namespace tpmodul4_1302220108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contoh_1 :");
+            KodePos kodepos = new KodePos();
+            kodepos.setKodePost();
+            kodepos.getKodePos();
+
+            Console.WriteLine("\nContoh_2 :");
+            KodePos1 kodepos1 = new KodePos1();
+            kodepos1.getKodePos();
+
+            Console.WriteLine("\nSoal 3 :");
+            DoorMachine door = new DoorMachine();
+            door.run();
+        }
+    }
+}
